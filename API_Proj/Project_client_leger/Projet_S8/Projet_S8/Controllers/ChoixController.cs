520d8d0bff7aa69a011e5cc9aa101299b5f922b8
AGOSSOU Roseline Précieuse Akpé Fifamey <roseline.agossou@groupe-esigelec.org>
2024-04-11 17:04:50 +0200

    Projet_Client_Leger

---
+++ API_Proj/Project_client_leger/Projet_S8/Projet_S8/Controllers/ChoixController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projet_S8.Controllers
+{
+    public class ChoixController : Controller
+    {
+        public IActionResult Choix_Methode()
+        {
+            return View();
+        }
+    }
+}
---
+++ API_Proj/Project_client_leger/Projet_S8/Projet_S8/Controllers/Methode.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projet_S8.Controllers
+{
+    public class Methode : Controller
+    {
+        public IActionResult M_Analytique()
+        {
+            return View();
+        }
+        public IActionResult Random_Forest()
+        {
+            return View();
+        }
+        public IActionResult KNN()
+        {
+            return View();
+        }
+        public IActionResult SVM()
+        {
+            return View();
+        }
+    }
+}
