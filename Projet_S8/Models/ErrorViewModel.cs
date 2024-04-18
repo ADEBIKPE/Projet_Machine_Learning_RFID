8e76bfe11df78c309c408f38f05f19c1503a5817
AGOSSOU Roseline Précieuse Akpé Fifamey <roseline.agossou@groupe-esigelec.org>
2024-04-18 14:32:54 +0200

    mise a jour version

--- Projet_S8/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Projet_S8.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
