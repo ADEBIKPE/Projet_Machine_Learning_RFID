e766c8b6c56cd492593b76463b26cc3407fe2762
Roselineee <roseline.agossou@groupe-esigelec.org>
2024-04-18 20:00:22 +0200

    version pre-finale du site web_GOODV

--- Site_web/Projet_S8/obj/Debug/net6.0/Projet_S8.AssemblyInfo.cs
+++ Site_web/Projet_S8/obj/Debug/net6.0/Projet_S8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projet_S8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+305903caf928790578a6dea0cf26fe19b08a7248")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projet_S8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projet_S8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
