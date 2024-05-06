24bbc8b4907648e212b8649c0aa70becf93297a3
GUINDO SAFIETOU <safietou.guindo@groupe-esigelec.org>
2024-05-06 12:20:11 +0200

    Ajout_Page_Connexion_Net.8

---
+++ Site_web/Site_Web_dern_version/Projet_Tech_Pag_Con/Projet_Tech_Pag_Con/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Projet_Tech_Pag_Con.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Site_web/Site_Web_dern_version/Projet_Tech_Pag_Con/Projet_Tech_Pag_Con/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projet_Tech_Pag_Con.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
